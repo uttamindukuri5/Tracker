b0513a6faab7ba60cbd0a299d2c26c00271f3627
uttamindukuri <uttamindukuri5@gmail.com>
2021-06-06 23:28:15 -0400

    enabled backend https

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,21 @@
+const https = require('https');
+const fs = require('fs');
 const AWS = require('./aws/aws');
 const app = require('./express');
 
 AWS.config.getCredentials(function(err) {
     if (err) console.log(err.stack);
     else {
-        console.log("Access key:", AWS.config.credentials.accessKeyId);
-        app.listen(4000, () => {
-            console.log('Server running at port 4000');
+
+        https.createServer({
+            key: fs.readFileSync('../../../../../../../etc/letsencrypt/live/vtwalk.org/privkey.pem'),
+            cert: fs.readFileSync('../../../../../../../etc/letsencrypt/live/vtwalk.org/fullchain.pem'),
+            passphrase: process.env.PASSPHRASE
+        }, app).listen(4000, () => {
+            console.log('Server has started in port 4000');
         });
+        // app.listen(4000, () => {
+        //     console.log('Server running at port 4000');
+        // });
     }
 });
