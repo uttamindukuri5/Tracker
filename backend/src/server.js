ef4e631a361176e7ec017a1e6c66a7067fe0e14c
uttamindukuri <uttamindukuri5@gmail.com>
2021-06-07 00:06:54 -0400

    updated path for backend cert

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,8 +7,8 @@ AWS.config.getCredentials(function(err) {
     if (err) console.log(err.stack);
     else {
         https.createServer({
-            key: fs.readFileSync('../../../../../../../etc/letsencrypt/live/vtwalk.org/privkey.pem'),
-            cert: fs.readFileSync('../../../../../../../etc/letsencrypt/live/vtwalk.org/fullchain.pem'),
+            key: fs.readFileSync('./privkey.pem'),
+            cert: fs.readFileSync('./fullchain.pem'),
             passphrase: process.env.PASSPHRASE
         }, app).listen(4000, () => {
             console.log('Server has started in port 4000');
